db2cdc16cd39857f0e2562e3f4ffdc14ba947f41
mikailb <115622678+mikailb@users.noreply.github.com>
2024-10-07 12:06:15 +0200

    lagt inn table, vieimport, navigtatioj
    fikset navigation bar og litt layout

--- InstagramMVC/Program.cs
+++ InstagramMVC/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute();
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
