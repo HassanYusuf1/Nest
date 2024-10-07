274379bf9c5c633eb2ee199804ed1917e596742c
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2024-10-07 12:01:01 +0200

    klasser

---
+++ InstagramMVC/Models/Innlegg.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Instagram.Models {
+    public class Innlegg {
+        public int Id {get; set;}  
+        public string BildeSti {get; set;}  // Lagrer filstien til bilde
+        public string BildeTekst {get; set;} // Bildetekst
+
+        public DateTime OpprettetDato {get; set;} // dato innlegget ble opprettet
+
+        public int BrukerId {get; set;} // Forhold til brukeren som eier innlegget 
+
+        public List <Kommentar> Kommentarer {get; set;} //Liste over kommentarer til innlegget
+
+    
+}
+
+}
