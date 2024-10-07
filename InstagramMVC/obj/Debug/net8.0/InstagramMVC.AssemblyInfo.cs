cc0650749029207548b45110d672e78684401423
mikailb <115622678+mikailb@users.noreply.github.com>
2024-10-07 12:34:44 +0200

    slettet

--- InstagramMVC/obj/Debug/net8.0/InstagramMVC.AssemblyInfo.cs
+++ InstagramMVC/obj/Debug/net8.0/InstagramMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstagramMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1da9ff0d3a67dd20e771ef4fc027a5122bb83a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be7772189d3f00ca010397e5b87d0f3304c23a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstagramMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstagramMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
