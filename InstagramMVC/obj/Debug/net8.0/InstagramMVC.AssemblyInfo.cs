bbc9f691ff034e8fca9a34a4b6a0eb24fc8965a8
mikailb <115622678+mikailb@users.noreply.github.com>
2024-10-07 13:46:12 +0200

    run

--- InstagramMVC/obj/Debug/net8.0/InstagramMVC.AssemblyInfo.cs
+++ InstagramMVC/obj/Debug/net8.0/InstagramMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstagramMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18ae969b4e56fb63cfabaa52db6e01bd2dfccc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+467b444ca40464ccd6aa2fa70fe5596de2d62546")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstagramMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstagramMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
