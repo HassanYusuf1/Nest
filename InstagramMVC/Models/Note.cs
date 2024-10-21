92a95c5678d5cc257386f8e878d7496e2284b4af
Andyyxd1 <114660172+Andyyxd1@users.noreply.github.com>
2024-10-21 11:46:27 +0200

    sletta for nå

--- InstagramMVC/Models/Note.cs
+++ InstagramMVC/Models/Note.cs
@@ -1,11 +1,12 @@
-namespace webappinsta.Models
+using System;
+
+namespace InstagramMVC.Models
 {
     public class Note
     {
         public int NoteId {get; set;}
         public string Tittel {get; set;}
         public string Innhold {get; set;}
-        public Datetime OpprettetDato {get; set;}
         public int BrukerId {get; set;}
         public List <Kommentar>? Kommentarer {get; set;}
     }
