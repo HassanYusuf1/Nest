b428d329b17f2b1655811bf7a56f64209fb70717
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2024-10-28 15:27:32 +0100

    Endring
    Endring Kommentar

--- InstagramMVC/Migrations/20241028142613_LeggTilKommentarFelt.Designer.cs
+++ InstagramMVC/Migrations/20241028142613_LeggTilKommentarFelt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InstagramMVC.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InstagramMVC.Migrations
 {
     [DbContext(typeof(MediaDbContext))]
-    partial class MediaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241028142613_LeggTilKommentarFelt")]
+    partial class LeggTilKommentarFelt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
