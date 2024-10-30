0f48b02965af017eb8c9a2c6a060845ce92f651a
Andyyxd1 <114660172+Andyyxd1@users.noreply.github.com>
2024-10-30 11:45:50 +0100

    Fikset warnings igjennn
    omg

--- InstagramMVC/Models/Note.cs
+++ InstagramMVC/Models/Note.cs
@@ -1,12 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace InstagramMVC.Models
 {
     public class Note
     {
         public int NoteId {get; set;}
-        public string Tittel {get; set;}
-        public string Innhold {get; set;}
+        [Required]
+        public string Tittel {get; set;} = string.Empty;
+        [Required]
+        public string Innhold {get; set;} = string.Empty;
         public int BrukerId {get; set;}
 
         public DateTime OpprettetDato {get; set;} // dato innlegget ble opprettet
