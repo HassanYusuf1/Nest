8a7ced7ca82f20ff28bc7b97353759563941972b
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2024-10-31 14:55:04 +0100

    Endring
    Lagt til UserName

---
+++ InstagramMVC/Migrations/20241031135344_AddUserNameToBilde.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstagramMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNameToBilde : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BrukerId",
+                table: "Bilder");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Bilder",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Bilder");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BrukerId",
+                table: "Bilder",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
